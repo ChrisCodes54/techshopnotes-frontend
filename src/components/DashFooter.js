e88d6508c3523d8778369680cc2425b313e8c51c
Chris <72607953+ChrisCodes54@users.noreply.github.com>
2023-06-21 16:34:59 -0400

    added components for public and private header dash, footer dash and welcome page

---
+++ src/components/DashFooter.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHouse } from '@fortawesome/free-solid-svg-icons'
+import { useNavigate, useLocation } from 'react-router-dom'
+
+
+const DashFooter = () => {
+    const navigate = useNavigate()
+    const { pathname } = useLocation()
+    const onGoHomeClick = () => navigate('/dash')
+
+    let goHomeButton = null 
+    if (pathname !== '/dash') {
+        goHomeButton = (
+            <button
+            className='dash-footer_button icon-button'
+            title='Home'
+            onClick={onGoHomeClick}
+            >
+            <FontAwesomeIcon icon={faHouse}/>
+            </button>
+        )
+    }
+
+    const content = (
+        {goHomeButton}
+        <footer className='dash-footer'>
+            <p>Current User:</p>
+            <p>Status:</p>
+
+        </footer>
+    )
+    return content
+
+}
+
+export default DashFooter
---
+++ src/components/DashHeader.js
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+
+const DashHeader = () => {
+  return (
+    <header className="dash-header">
+        <div className="dash-header_container">
+            <Link to="/dash/notes">
+            <h1 className="dash-header_title">techNotes</h1>
+            </Link>
+            <nav className="dash-header_nav">
+                {/* buttons will go here */}
+            </nav>
+        </div>
+    </header>
+  )
+}
+
+export default DashHeader
